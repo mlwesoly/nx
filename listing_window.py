63a4846f5e0c2b013f980e186a478edbf15282e9
michimuellae <micha@pumbakumba.com>
2021-04-04 14:50:57 +0200

    first three scripts commited

---
+++ listing_window.py
@@ -0,0 +1,17 @@
+﻿import NXOpen
+
+def main():
+
+    theSession  = NXOpen.Session.GetSession()
+    theUI = NXOpen.UI.GetUI()
+    
+    lw = theSession.ListingWindow
+    # Insert code here 
+    lw.Open()
+    
+    lw.WriteLine("hello")
+    lw.Close()
+    
+if __name__ == '__main__':
+    main()
+
