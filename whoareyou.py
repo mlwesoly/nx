d4f607c524082e538efeb3e74581a537838ffe56
Michael Wesoly <mlwesoly@pm.me>
2022-01-03 20:28:13 +0000

    Update whoareyou.py

--- whoareyou.py
+++ whoareyou.py
@@ -1,14 +1,16 @@
-﻿import re
+import re
 import NXOpen
 import NXOpen.Annotations
 import NXOpen.Drafting
 import NXOpen.Drawings
 import NXOpen.UF
 
+
 def main() :
     debug = 0
     theSession  = NXOpen.Session.GetSession()
     workPart = theSession.Parts.Work
+    theUFSession = NXOpen.UF.UFSession.GetUFSession()
     theUI = NXOpen.UI.GetUI()
     lw = theSession.ListingWindow
     
@@ -30,6 +32,10 @@ def main() :
         selObj = theUI.SelectionManager.GetSelectedObject(i)
         objType = type(theUI.SelectionManager.GetSelectedObject(i))
         lw.WriteLine(str(objType))
+        for dir_obj in dir(i):
+            lw.WriteLine(str(dir_obj))
+        lw.WriteLine(str(i.Name))
+        lw.WriteLine("\n")
         #lw.WriteLine(str(isinstance(objType, NXOpen.Annotations.Note)))
         #if isinstance(objType, NXOpen.Annotations.Note):
         #    lw.WriteLine("jo")
@@ -38,6 +44,15 @@ def main() :
         #if selObj.GetText()[0].find("VWNAME")>0:
         #    lw.WriteLine(str(selObj.GetText()))
         lw.WriteLine(selObj.SectionViewParent.Name)
+    
+    # ajdust this part, for make it working in general
+    #for sheet in workPart.DrawingSheets:
+    #sheet.Open()
+    #for obj in sheet.View.AskVisibleObjects(): 
+    #        for j in dir(obj):
+    #            lw.WriteLine(str(j))
+    #        lw.WriteLine(str(obj.Name))
+    #        lw.WriteLine("\n")
   
     lw.Close()
     # theSession.CleanUpFacetedFacesAndEdges()
